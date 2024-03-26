2a6ea9dc73e0c40eff18f38ca8d3fc616ddcce22
KumarsantoshPRO <kumar.santosh@protivitiglobal.in>
2024-03-26 11:44:09 +0530

    supply chain deletion

--- zpro_supplychain/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["zpj/pro/sk/sd/supplychain/zprosupchain/zprosupplychain/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- zpro_supplychain/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"zpjprosksdsupplychainzprosupchain/zpro_supplychain/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
