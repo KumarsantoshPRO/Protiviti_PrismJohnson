b736ed3c6c32bf672151c359f16a43bd36760d4f
KumarsantoshPRO <kumar.santosh@protivitiglobal.in>
2024-03-26 18:51:16 +0530

    ED Changes

---
+++ zpro_exdirector/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["zpj/pro/sd/sk/zproexdirector/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ zpro_exdirector/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"zpjprosdsk/zpro_exdirector/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
