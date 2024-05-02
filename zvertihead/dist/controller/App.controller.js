81e38859fbfad6dfc2130c0ec7d02233497efbdb
KumarsantoshPRO <kumar.santosh@protivitiglobal.in>
2024-05-02 12:16:53 +0530

    Deleted unwanted project

--- zvertihead/dist/controller/App.controller.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/mvc/Controller"],function(e){"use strict";return e.extend("zpj.pro.sd.sk.zprovertihead.controller.App",{onInit:function(){}})});
-//# sourceMappingURL=App.controller.js.map
